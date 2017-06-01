b14182a468d3cc3f82e241f941c2b22c88022755
cuti37 <cuti.it@hotmail.com>
2017-06-01 08:14:15 +0700

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -1,21 +1,25 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
 
-gem "rails", "~> 5.0.3"
+gem "bcrypt", "3.1.11"
+gem "bootstrap-sass", "3.3.6"
+gem "bootstrap-will_paginate", "1.0.0"
+gem "coffee-rails", "~> 4.2"
+gem "config"
+gem "faker","1.7.3"
+gem "jbuilder", "~> 2.5"
+gem "jquery-rails"
 gem "mysql2", ">= 0.3.18", "< 0.5"
 gem "puma", "~> 3.0"
+gem "rails", "~> 5.0.3"
 gem "sass-rails", "~> 5.0"
-gem "bootstrap-sass", "3.3.6"
-gem "uglifier", ">= 1.3.0"
-gem "coffee-rails", "~> 4.2"
-gem "jquery-rails"
 gem "turbolinks", "~> 5"
-gem "jbuilder", "~> 2.5"
-
+gem "uglifier", ">= 1.3.0"
+gem "will_paginate", "3.1.5"
 group :development, :test do
   gem "byebug", platform: :mri
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,81 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :load_user, only: :show
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.select(:id, :name, :email).order(:id)
+      .paginate page: params[:page], per_page: Settings.user.user_per_page
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".welcome"
+      redirect_to @user
+    else
+      flash[:danger] = t ".error"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".update_success"
+      redirect_to @user
+    else
+      flash[:danger] = t ".update_error"
+      render :edit
+    end
+  end
+
+  def destroy
+    flash[:success] = t ".delete_success"
+    redirect_to users_url
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
+
+    def load_user
+      @user = User.find_by id: params[:id]
+
+      if @user.nil?
+        flash[:warning] = t ".not_found"
+        redirect_to root_path
+      end
+    end
+
+    def correct_user
+      @user = User.find_by id: params[:id]
+      redirect_to root_url unless @user.is? @user
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = t ".log_in_please"
+        redirect_to login_url
+      end
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
+end
